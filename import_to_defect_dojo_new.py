90815477ccba61dbdfe7be6e6ffef309cd049550
0xInyiak <51320237+0xInyiak@users.noreply.github.com>
2024-09-04 15:41:40 +0700

    Update import_to_defect_dojo_new.py

--- import_to_defect_dojo_new.py
+++ import_to_defect_dojo_new.py
@@ -8,12 +8,13 @@ proxy = {
     'https': 'http://127.0.0.1:8080'
 }
 
-def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename, scan_type):
+def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename, scan_type,test_title):
     multipart_form_data = {
         'file': (filename, open(filename, 'rb')),
         'scan_type': (None, scan_type),
         'product_name': (None, product_name),
         'engagement_name': (None, engagement_name),
+        'test_title': (None, test_title)
     }
 
     endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
@@ -46,7 +47,8 @@ if __name__ == "__main__":
         engagement_name = sys.argv[6]
         report = sys.argv[8]
         scan_type = sys.argv[10]
-        uploadToDefectDojo(False, token, url, product_name, engagement_name, report,scan_type)
+        test_title = sys.argv[12]
+        uploadToDefectDojo(False, token, url, product_name, engagement_name, report,scan_type,test_title)
     else:
         print(
             'Usage: python3 import_to_defect_dojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE --scan-type SCAN_TYPE')
